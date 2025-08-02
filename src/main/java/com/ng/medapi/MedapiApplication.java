3da3ca196aa00f9036fc5d906553a4aca19ed569
Nico <nrgaleano00@gmail.com>
2025-08-02 02:40:31 -0300

    Commit inicial

---
+++ src/main/java/com/ng/medapi/MedapiApplication.java
@@ -0,0 +1,13 @@
+package com.ng.medapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedapiApplication.class, args);
+	}
+
+}
