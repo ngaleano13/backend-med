9c54905f9b8f0f112fd343a6bf81c487c9eaa799
Nico <nrgaleano00@gmail.com>
2025-08-06 21:09:05 -0300

    Modificacion Manejo de DTO

--- src/main/java/com/ng/medapi/Controllers/UsuarioController.java
+++ src/main/java/com/ng/medapi/Controllers/UsuarioController.java
@@ -1,9 +1,9 @@
 package com.ng.medapi.Controllers;
 
-import java.security.Principal;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ng.medapi.Dtos.UsuarioDTO;
 import com.ng.medapi.Models.Usuario;
 import com.ng.medapi.Services.UsuarioService;
+import com.ng.medapi.Utils.UsuarioMapper;
 
 import lombok.AllArgsConstructor;
 
@@ -23,6 +25,7 @@ import lombok.AllArgsConstructor;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final UsuarioMapper usuarioMapper;
 
     @PostMapping("/registro")
     public ResponseEntity<Usuario> registrarUsuario(@RequestBody Usuario usuario) {
@@ -32,9 +35,10 @@ public class UsuarioController {
 
     @GetMapping("/perfil")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<Usuario> verPerfil(Principal principal) {
-        Usuario usuario = usuarioService.buscarPorEmail(principal.getName());
-        return ResponseEntity.ok(usuario);
+    public ResponseEntity<UsuarioDTO> verPerfil(@AuthenticationPrincipal UserDetails userDetails) {
+        Usuario usuario = usuarioService.buscarPorEmail(userDetails.getUsername());
+        UsuarioDTO dto = usuarioMapper.toDTO(usuario);
+        return ResponseEntity.ok(dto);
     }
 
     @PutMapping("/{id}")
