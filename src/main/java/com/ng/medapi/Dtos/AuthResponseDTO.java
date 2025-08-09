3e8b9dec569af983cf1ccde73bcadc10e0e937bf
Nico <nrgaleano00@gmail.com>
2025-08-09 18:40:18 -0300

    Agregar AuthDTO

---
+++ src/main/java/com/ng/medapi/Dtos/AuthResponseDTO.java
@@ -0,0 +1,14 @@
+package com.ng.medapi.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDTO {
+    private String email;
+    private String rol;
+}
+
