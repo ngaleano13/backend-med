972acc6b7242921fdd28221c96827ec6e479b78f
Nico <nrgaleano00@gmail.com>
2025-08-09 18:41:03 -0300

    Actualizar Usuario por ID (ADMIN) y Email (USER)

--- src/main/java/com/ng/medapi/Services/UsuarioService.java
+++ src/main/java/com/ng/medapi/Services/UsuarioService.java
@@ -2,14 +2,16 @@ package com.ng.medapi.Services;
 
 import java.util.List;
 
+import com.ng.medapi.Dtos.UsuarioUpdateDTO;
 import com.ng.medapi.Models.Usuario;
 
 public interface UsuarioService {
     List<Usuario> listarUsuarios();
     Usuario buscarPorId(Long id);
     Usuario buscarPorDocumento(String documento);
-    Usuario crearUsuario (Usuario usuario);
-    Usuario actualizarUsuario(Long id, Usuario Usuario);
+    Usuario crearUsuario(Usuario usuario);
+    Usuario actualizarUsuario(String email, UsuarioUpdateDTO datosNuevos);
+    Usuario actualizarUsuarioPorId(Long id, UsuarioUpdateDTO datosNuevos);
     Usuario actualizarRol(Long id);
     void eliminarUsuario(Long id);
     Usuario buscarPorEmail(String email);
