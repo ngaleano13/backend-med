ee3a4a2ef1c45bcc960d74504b745328b512dcf0
Nico <nrgaleano00@gmail.com>
2025-08-09 18:35:47 -0300

    Actualizar Modificar Rol y Usuario

--- src/main/java/com/ng/medapi/Controllers/AdminController.java
+++ src/main/java/com/ng/medapi/Controllers/AdminController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ng.medapi.Dtos.UsuarioDTO;
+import com.ng.medapi.Dtos.UsuarioUpdateDTO;
 import com.ng.medapi.Models.Usuario;
 import com.ng.medapi.Services.UsuarioService;
+import com.ng.medapi.Utils.UsuarioMapper;
 
 import lombok.AllArgsConstructor;
 
@@ -22,6 +26,7 @@ import lombok.AllArgsConstructor;
 public class AdminController {
 
     private final UsuarioService usuarioService;
+    private final UsuarioMapper usuarioMapper;
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
@@ -35,7 +40,18 @@ public class AdminController {
         return ResponseEntity.ok(usuarioService.buscarPorId(id));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/actualizar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UsuarioDTO> actualizarUsuarioComoAdmin(
+            @PathVariable Long id,
+            @RequestBody UsuarioUpdateDTO usuarioUpdateDTO) {
+
+        Usuario actualizado = usuarioService.actualizarUsuarioPorId(id, usuarioUpdateDTO);
+        UsuarioDTO dto = usuarioMapper.toDTO(actualizado);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping("/modificarRol/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Usuario> acutalizarAdmin(@PathVariable Long id) {
         Usuario actualizado = usuarioService.actualizarRol(id);
